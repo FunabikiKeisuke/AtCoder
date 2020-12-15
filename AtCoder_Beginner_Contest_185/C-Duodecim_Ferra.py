2da27c1a89b06c2306874801fd4f0bbda79c8dc3
FunabikiKeisuke <bickey_t0mat0@outlook.jp>
2020-12-16 00:22:29 +0900

    C 問題の py 解法を追加

--- AtCoder_Beginner_Contest_185/C-Duodecim_Ferra.py
+++ AtCoder_Beginner_Contest_185/C-Duodecim_Ferra.py
@@ -1,23 +1,14 @@
-from operator import mul
-from functools import reduce
-
-
-def cmb(n, r):
-    r = min(n-r, r)
-    if r == 0:
-        return 1
-    over = reduce(mul, range(n, n - r, -1))
-    under = reduce(mul, range(1, r + 1))
-    return over // under
+from math import comb
 
 
 def main():
+    """
+    長さ L の棒の分割後の各棒の長さが全て正整数になるためには，整数長さ以外の場所を切ってはいけない．
+    切る位置の候補は L-1 個あり，そこから異なる 11 箇所を選ぶ場合の数を求めれば良い．
+    これは comb(L-1, 11) で求められる．
+    """
     l_input = int(input())
-    surplus = l_input - 12
-    ans = 0
-    if surplus == 0:
-        return 1
-
+    ans = comb(l_input-1, 11)
     return ans
 
 
