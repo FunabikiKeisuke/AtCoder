a5571f0c9e0459c77354e3ed865bbf3c6a8eabf9
FunabikiKeisuke <bickey_t0mat0@outlook.jp>
2020-12-16 23:36:14 +0900

    comb 関数の修正

--- AtCoder_Beginner_Contest_185/C-Duodecim_Ferra.cpp
+++ AtCoder_Beginner_Contest_185/C-Duodecim_Ferra.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <typeinfo>
 using namespace std;
 
 long long comb(int n, int r) {
+  n += 1;
   long long ncr = 1;
   for (int i = 1; i <= r; i++) {
     ncr *= n - i;  // nCr の分子
@@ -15,7 +15,7 @@ int main() {
   int l_input;
   cin >> l_input;
   long long ans;
-  ans = comb(l_input, 11);
+  ans = comb(l_input-1, 11);
   cout << ans << endl;
 
   return 0;
