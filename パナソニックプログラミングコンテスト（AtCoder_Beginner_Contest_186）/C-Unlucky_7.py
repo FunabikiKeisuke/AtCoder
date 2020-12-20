d3b3c527410358ec19e0554b23788dd0bb9f07f0
FunabikiKeisuke <bickey_t0mat0@outlook.jp>
2020-12-20 22:38:50 +0900

    条件式を簡略化して処理速度を向上させた

--- パナソニックプログラミングコンテスト（AtCoder_Beginner_Contest_186）/C-Unlucky_7.py
+++ パナソニックプログラミングコンテスト（AtCoder_Beginner_Contest_186）/C-Unlucky_7.py
@@ -1,8 +1,7 @@
 n_input = int(input())
-array = list(range(1, n_input+1, 1))
-str_array = [str(s) for s in array]
-not_in_7 = [i for i in str_array if "7" not in i]
-int_array = [int(s) for s in not_in_7]
-str_oct_array = [oct(s) for s in int_array]
-not_in_7 = [i for i in str_oct_array if "7" not in i]
-print(len(not_in_7))
+ans = 0
+for i in range(1, n_input+1):
+    if ('7' not in str(i)) and ('7' not in oct(i)):
+        ans += 1
+
+print(ans)
